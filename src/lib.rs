79a84ff24fe96827d751a4130cb81b5dda301f8d
dorayaki4369 <dorayaki4369@outlook.jp>
2023-05-17 04:08:10 +0900

    remove unwrap

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::error;
 use nom::bytes::complete::take;
 use nom::error::Error;
 use nom::number::complete::le_u32;
@@ -91,88 +92,88 @@ pub enum SkeletonOrFrame {
 }
 
 /// Parse the values.
-fn parse_value(data: &[u8]) -> Data {
+fn parse_value(data: &[u8]) -> Result<Data, Box<dyn error::Error + '_>> {
     // lengthの長さは4bytesで固定
-    let (data, length) = le_u32::<_, Error<_>>(data).unwrap() as (&[u8], u32);
+    let (data, length) = le_u32::<_, Error<_>>(data)? as (&[u8], u32);
 
     // nameは4bytesの文字列
-    let (data, name) = take::<_, _, Error<_>>(4usize)(data).unwrap();
-    let name_str = String::from_utf8(name.to_vec()).unwrap();
+    let (data, name) = take::<_, _, Error<_>>(4usize)(data)?;
+    let name_str = String::from_utf8(name.to_vec())?;
 
     // valueの長さはlengthの値による
-    let (rem, data) = take::<_, _, Error<_>>(length)(data).unwrap();
+    let (rem, data) = take::<_, _, Error<_>>(length)(data)?;
 
-    return Data {
+    Ok(Data {
         len: length,
         name: name_str,
         data,
         rem,
-    };
+    })
 }
 
-fn parse_head(data: &[u8]) -> (u32, Head) {
-    let data = parse_value(data);
+fn parse_head(data: &[u8]) -> Result<(u32, Head), Box<dyn error::Error + '_>> {
+    let data = parse_value(data)?;
     let len = data.len;
 
     // ftyp
-    let data = parse_value(data.data);
-    let format = String::from_utf8(data.data.to_vec()).unwrap();
+    let data = parse_value(data.data)?;
+    let format = String::from_utf8(data.data.to_vec())?;
 
     // vrsn
-    let data = parse_value(data.rem);
+    let data = parse_value(data.rem)?;
     let ver = data.data[0];
 
-    (len, Head { format, ver })
+    Ok((len, Head { format, ver }))
 }
 
-fn parse_info(data: &[u8]) -> (u32, Info) {
-    let data = parse_value(data);
+fn parse_info(data: &[u8]) -> Result<(u32, Info), Box<dyn error::Error + '_>> {
+    let data = parse_value(data)?;
     let len = data.len;
 
     // ipad
-    let data = parse_value(data.data);
-    let addr = u64::from_le_bytes(data.data.try_into().unwrap());
+    let data = parse_value(data.data)?;
+    let addr = u64::from_le_bytes(data.data.try_into()?);
 
     // rcvp
-    let data = parse_value(data.rem);
-    let port = u16::from_le_bytes(data.data.try_into().unwrap());
+    let data = parse_value(data.rem)?;
+    let port = u16::from_le_bytes(data.data.try_into()?);
 
-    (len, Info { addr, port })
+    Ok((len, Info { addr, port }))
 }
 
-fn parse_skeleton(data: &[u8]) -> (u32, Skeleton) {
+fn parse_skeleton(data: &[u8]) -> Result<(u32, Skeleton), Box<dyn error::Error + '_>> {
     // skdf
-    let data = parse_value(data);
+    let data = parse_value(data)?;
     let len = data.len;
 
     // bons
-    let (_, bones) = parse_bones(data.data);
+    let (_, bones) = parse_bones(data.data)?;
 
-    (len, Skeleton { bones: *bones })
+    Ok((len, Skeleton { bones: *bones }))
 }
 
-fn parse_frame(data: &[u8]) -> (u32, Frame) {
+fn parse_frame(data: &[u8]) -> Result<(u32, Frame), Box<dyn error::Error + '_>> {
     // fram
-    let data = parse_value(data);
+    let data = parse_value(data)?;
     let len = data.len;
 
     // fnum
-    let data = parse_value(data.data);
-    let num = u32::from_le_bytes(data.data.try_into().unwrap());
+    let data = parse_value(data.data)?;
+    let num = u32::from_le_bytes(data.data.try_into()?);
 
     // time
-    let data = parse_value(data.rem);
-    let time = u32::from_le_bytes(data.data.try_into().unwrap());
+    let data = parse_value(data.rem)?;
+    let time = u32::from_le_bytes(data.data.try_into()?);
 
     // btrs
-    let (_, bones) = parse_bone_trans(data.rem);
+    let (_, bones) = parse_bone_trans(data.rem)?;
 
-    (len, Frame { num, time, bones: *bones })
+    Ok((len, Frame { num, time, bones: *bones }))
 }
 
-fn parse_bone_trans(data: &[u8]) -> (u32, Box<Vec<BoneTrans>>) {
+fn parse_bone_trans(data: &[u8]) -> Result<(u32, Box<Vec<BoneTrans>>), Box<dyn error::Error + '_>> {
     // btrs
-    let btrs_data = parse_value(data);
+    let btrs_data = parse_value(data)?;
     let btrs_len = btrs_data.len;
 
     // btrsの下にあるbtdtをparseしていく
@@ -182,15 +183,15 @@ fn parse_bone_trans(data: &[u8]) -> (u32, Box<Vec<BoneTrans>>) {
         let part = &btrs_data.data[(read_bytes as usize)..];
 
         // btdt
-        let data = parse_value(part);
+        let data = parse_value(part)?;
         let len = data.len;
 
         // bnid
-        let data = parse_value(data.data);
-        let id = u16::from_le_bytes(data.data.try_into().unwrap());
+        let data = parse_value(data.data)?;
+        let id = u16::from_le_bytes(data.data.try_into()?);
 
         // tran
-        let (_, trans) = parse_trans(data.rem);
+        let (_, trans) = parse_trans(data.rem)?;
 
         bones.push(BoneTrans { id, trans });
 
@@ -200,12 +201,12 @@ fn parse_bone_trans(data: &[u8]) -> (u32, Box<Vec<BoneTrans>>) {
         }
     }
 
-    (btrs_len, Box::new(bones))
+    Ok((btrs_len, Box::new(bones)))
 }
 
-fn parse_bones(data: &[u8]) -> (u32, Box<Vec<Bone>>) {
+fn parse_bones(data: &[u8]) -> Result<(u32, Box<Vec<Bone>>), Box<dyn error::Error + '_>> {
     // bons
-    let bons_data = parse_value(data);
+    let bons_data = parse_value(data)?;
     let bons_len = bons_data.len;
 
     // bonsの下にあるbndtをparseしていく
@@ -215,19 +216,19 @@ fn parse_bones(data: &[u8]) -> (u32, Box<Vec<Bone>>) {
         let part = &bons_data.data[(read_bytes as usize)..];
 
         // bndt
-        let data = parse_value(part);
+        let data = parse_value(part)?;
         let len = data.len;
 
         // bnid
-        let data = parse_value(data.data);
-        let id = u16::from_le_bytes(data.data.try_into().unwrap());
+        let data = parse_value(data.data)?;
+        let id = u16::from_le_bytes(data.data.try_into()?);
 
         // pbid
-        let data = parse_value(data.rem);
-        let parent = u16::from_le_bytes(data.data.try_into().unwrap());
+        let data = parse_value(data.rem)?;
+        let parent = u16::from_le_bytes(data.data.try_into()?);
 
         // tran
-        let (_, trans) = parse_trans(part);
+        let (_, trans) = parse_trans(part)?;
 
         bones.push(Bone { id, parent, trans });
 
@@ -237,25 +238,25 @@ fn parse_bones(data: &[u8]) -> (u32, Box<Vec<Bone>>) {
         }
     }
 
-    (bons_len, Box::new(bones))
+    Ok((bons_len, Box::new(bones)))
 }
 
-fn parse_trans(data: &[u8]) -> (u32, Transform) {
+fn parse_trans(data: &[u8]) -> Result<(u32, Transform), Box<dyn error::Error + '_>> {
     // tran
-    let data = parse_value(data);
+    let data = parse_value(data)?;
     let len = data.len;
 
     // 28bytesのデータを4bytesごとに取り出す
     let mut values = [0.0; 7];
     for i in 0..6usize {
-        let v = data.data[i * 4..(i * 4 + 4)].to_vec();
-        values[i] = f32::from_le_bytes(v.try_into().unwrap());
+        let v = &data.data[i * 4..(i * 4 + 4)];
+        values[i] = f32::from_le_bytes(v.try_into()?);
     }
 
-    (len, Transform {
+    Ok((len, Transform {
         rot: Rotation { x: values[0], y: values[1], z: values[2], w: values[3] },
         pos: Position { x: values[4], y: values[5], z: values[6] },
-    })
+    }))
 }
 
 /// Parse the streamed data from mocopi.
@@ -270,25 +271,25 @@ fn parse_trans(data: &[u8]) -> (u32, Transform) {
 ///
 /// loop {
 ///     socket.recv_from(&mut buf).unwrap();
-///     let packet = mocopi_parser::parse(&mut buf);
+///     let packet = mocopi_parser::parse(&mut buf).unwrap();
 ///     println!("{:?}", packet);
 /// }
 /// ```
-pub fn parse(data: &mut [u8]) -> SkeletonOrFrame {
-    let (len, head) = parse_head(data);
+pub fn parse(data: &mut [u8]) -> Result<SkeletonOrFrame, Box<dyn error::Error + '_>> {
+    let (len, head) = parse_head(data)?;
     let mut remain = &data[((len + 8) as usize)..];
 
-    let (len, info) = parse_info(remain);
+    let (len, info) = parse_info(remain)?;
     remain = &remain[((len + 8) as usize)..];
 
-    let name = parse_value(data).name;
+    let name = parse_value(data)?.name;
 
     if name == "skdf" {
-        let (_, skeleton) = parse_skeleton(remain);
-        SkeletonOrFrame::Skeleton(SkeletonPacket { head, info, skeleton })
+        let (_, skeleton) = parse_skeleton(remain)?;
+        Ok(SkeletonOrFrame::Skeleton(SkeletonPacket { head, info, skeleton }))
     }
     else {
-        let (_, frame) = parse_frame(remain);
-        SkeletonOrFrame::Frame(FramePacket { head, info, frame })
+        let (_, frame) = parse_frame(remain)?;
+        Ok(SkeletonOrFrame::Frame(FramePacket { head, info, frame }))
     }
 }
